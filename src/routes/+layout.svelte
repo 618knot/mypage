d7cfda80e0cfa5426e2e777032e59cf64a5e5248
618knot <publicac.clt618knot@gmail.com>
2024-12-07 14:55:30 +0900

    ogp画像を設定

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,10 @@
 	let { children } = $props();
 
 	import Footer from '$lib/components/Footer.svelte';
+	import { page } from '$app/stores';
 </script>
+
+<meta property="og:image" content="{$page.url.origin}/og-image.jpg" />
 
 <div class="flex flex-col h-lvh">
 	{@render children()}
