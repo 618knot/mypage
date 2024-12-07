75abf38f8cc0fe7c8e7e7202837a5c0c8bfa2b0c
618knot <publicac.clt618knot@gmail.com>
2024-12-07 15:17:44 +0900

    ogp修正

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,10 @@
 
 	import Footer from '$lib/components/Footer.svelte';
 	import { page } from '$app/stores';
+	import { base } from '$app/paths';
 </script>
 
-<meta property="og:image" content="{$page.url.origin}/og-image.jpg" />
+<meta property="og:image" content="{$page.url.origin}{base}/og-image.png" />
 
 <div class="flex flex-col h-lvh">
 	{@render children()}
